10855830aa0b5a7a6271c31da51970f4c22265b0
Fabio Macieira <fabiomacieira@pmenos.com.br>
2021-12-27 17:54:28 -0300

    create head, body

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ecommerce_ReactJS_Redux_with_Fetch
  
+
+ # Ferramenta instaladas
+ - npm i bootstrap
+ - npm i font-awesom
+ - npm i redux react-redux
+ - npm i react-router-dom
---
+++ emart/src/component/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" class="card-img" alt="Background"
+                    heinght="550px" />
+                <div class="card-img-overlay d-flex flex-column justify-content-arrond">
+                    <div className="container-body">
+                        <h5 class="card-title display-3 fw-border md-o color:black">NEW SEASON ARRIVALS </h5>
+                        <p class="card-text">
+                            CHECK OUT ALL THE TRENDS                        
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
+
