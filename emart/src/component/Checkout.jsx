94db551a5c4a12e7dffaab9ceb7c05ceb92daf5a
Fabio Macieira <fabiomacieira@pmenos.com.br>
2022-01-06 20:07:15 -0300

    Final project

--- emart/src/component/Checkout.jsx
+++ emart/src/component/Checkout.jsx
@@ -10,7 +10,7 @@ const Checkout = () => {
 
     const itemList = (item) => {
         subtotal = formatPrice(item.qty * item.price);
-        total = total + item.qty * item.price;
+        total = ( total + item.qty * item.price);
         
 
         return (
@@ -52,23 +52,23 @@ const Checkout = () => {
                     <form className="needs-validation was-validated" />
                     <div className="row g-3">
                         <div className="col-sm-6">
-                            <label htmlfor="firstName" className="form-label">First name</label>
-                            <input type="text" className="form-control" id="firstName" placeholder="" value="" required="" />
+                            <label htmlFor="firstName" className="form-label">First name</label>
+                            <input type="text" className="form-control" id="firstName" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Valid first name is required.
                             </div>
                         </div>
 
                         <div className="col-sm-6">
-                            <label htmlfor="lastName" className="form-label">Last name</label>
-                            <input type="text" className="form-control" id="lastName" placeholder="" value="" required="" />
+                            <label htmlFor="lastName" className="form-label">Last name</label>
+                            <input type="text" className="form-control" id="lastName" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Valid last name is required.
                             </div>
                         </div>
 
                         <div className="col-12">
-                            <label htmlfor="username" className="form-label">Username</label>
+                            <label htmlFor="username" className="form-label">Username</label>
                             <div className="input-group has-validation">
                                 <span className="input-group-text">@</span>
                                 <input type="text" className="form-control" id="username" placeholder="Username" required="" />
@@ -79,7 +79,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-12">
-                            <label htmlfor="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
+                            <label htmlFor="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
                             <input type="email" className="form-control" id="email" placeholder="you@example.com" />
                             <div className="invalid-feedback">
                                 Please enter a valid email address for shipping updates.
@@ -87,7 +87,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-12">
-                            <label htmlfor="address" className="form-label">Address</label>
+                            <label htmlFor="address" className="form-label">Address</label>
                             <input type="text" className="form-control" id="address" placeholder="1234 Main St" required="" />
                             <div className="invalid-feedback">
                                 Please enter your shipping address.
@@ -95,14 +95,14 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-12">
-                            <label htmlfor="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
+                            <label htmlFor="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
                             <input type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
                         </div>
 
                         <div className="col-md-5">
-                            <label htmlfor="country" className="form-label">Country</label>
+                            <label htmlFor="country" className="form-label">Country</label>
                             <select className="form-select" id="country" required="">
-                                <option value="">Choose...</option>
+                                <option >Choose...</option>
                                 <option>United States</option>
                             </select>
                             <div className="invalid-feedback">
@@ -111,9 +111,9 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-md-4">
-                            <label htmlfor="state" className="form-label">State</label>
+                            <label htmlFor="state" className="form-label">State</label>
                             <select className="form-select" id="state" required="">
-                                <option value="">Choose...</option>
+                                <option >Choose...</option>
                                 <option>California</option>
                             </select>
                             <div className="invalid-feedback">
@@ -122,7 +122,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-md-3">
-                            <label htmlfor="zip" className="form-label">Zip</label>
+                            <label htmlFor="zip" className="form-label">Zip</label>
                             <input type="text" className="form-control" id="zip" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Zip code required.
@@ -134,12 +134,12 @@ const Checkout = () => {
 
                     <div className="form-check">
                         <input type="checkbox" className="form-check-input" id="same-address" />
-                        <label className="form-check-label" htmlfor="same-address">Shipping address is the same as my billing address</label>
+                        <label className="form-check-label" htmlFor="same-address">Shipping address is the same as my billing address</label>
                     </div>
 
                     <div className="form-check">
                         <input type="checkbox" className="form-check-input" id="save-info" />
-                        <label className="form-check-label" htmlfor="save-info">Save this information for next time</label>
+                        <label className="form-check-label" htmlFor="save-info">Save this information for next time</label>
                     </div>
 
                     <hr className="my-4" />
@@ -148,22 +148,22 @@ const Checkout = () => {
 
                     <div className="my-3">
                         <div className="form-check">
-                            <input id="credit" name="paymentMethod" type="radio" className="form-check-input" checked="" required="" />
-                            <label className="form-check-label" htmlfor="credit">Credit card</label>
+                            <input id="credit" name="paymentMethod" type="radio" className="form-check-input" required="" />
+                            <label className="form-check-label" htmlFor="credit">Credit card</label>
                         </div>
                         <div className="form-check">
                             <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required="" />
-                            <label className="form-check-label" htmlfor="debit">Debit card</label>
+                            <label className="form-check-label" htmlFor="debit">Debit card</label>
                         </div>
                         <div className="form-check">
                             <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required="" />
-                            <label className="form-check-label" htmlfor="paypal">PayPal</label>
+                            <label className="form-check-label" htmlFor="paypal">PayPal</label>
                         </div>
                     </div>
 
                     <div className="row gy-3">
                         <div className="col-md-6">
-                            <label htmlfor="cc-name" className="form-label">Name on card</label>
+                            <label htmlFor="cc-name" className="form-label">Name on card</label>
                             <input type="text" className="form-control" id="cc-name" placeholder="" required="" />
                             <small className="text-muted">Full name as displayed on card</small>
                             <div className="invalid-feedback">
@@ -172,7 +172,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-md-6">
-                            <label htmlfor="cc-number" className="form-label">Credit card number</label>
+                            <label htmlFor="cc-number" className="form-label">Credit card number</label>
                             <input type="text" className="form-control" id="cc-number" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Credit card number is required
@@ -180,7 +180,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-md-3">
-                            <label htmlfor="cc-expiration" className="form-label">Expiration</label>
+                            <label htmlFor="cc-expiration" className="form-label">Expiration</label>
                             <input type="text" className="form-control" id="cc-expiration" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Expiration date required
@@ -188,7 +188,7 @@ const Checkout = () => {
                         </div>
 
                         <div className="col-md-3">
-                            <label htmlfor="cc-cvv" className="form-label">CVV</label>
+                            <label htmlFor="cc-cvv" className="form-label">CVV</label>
                             <input type="text" className="form-control" id="cc-cvv" placeholder="" required="" />
                             <div className="invalid-feedback">
                                 Security code required
